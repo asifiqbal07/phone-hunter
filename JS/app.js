dea85d6573aac6598e3a5e6f6d40edc7ef4075c7
asifiqbal07 <as.asif.07@gmail.com>
2022-09-01 21:30:27 +0600

    Js updated

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,23 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
 
     // Display 20 Phones Only //
-    phones = phones.slice(0, 20);
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else {
+        showAll.classList.add('d-none');
+    }
 
     // Display No Phone Found //
     const noPhone = document.getElementById('no-phone-found');
@@ -35,6 +42,7 @@ const displayPhones = phones => {
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content.</p>
+                                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                         </div>
                     </div>
         `;
@@ -43,15 +51,25 @@ const displayPhones = phones => {
     });
 }
 
+const processSearch = (dataLimit) => {
+    toogleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText, dataLimit);
+}
 
 
 document.getElementById('btn-search').addEventListener('click', function () {
     // Start Loader //
-    toogleSpinner(true);
-    const searchField = document.getElementById('exampleFormControlInput1');
-    const searchText = searchField.value;
-    loadPhone(searchText);
-    searchField.value = '';
+    processSearch(10);
+})
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        // code for enter
+        processSearch(10);
+    }
 })
 
 const toogleSpinner = isLoading => {
@@ -64,5 +82,25 @@ const toogleSpinner = isLoading => {
     }
 
 }
+// Not the best way to load Show All //
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+})
 
-// loadPhone();
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const modalTittle = document.getElementById('phoneDetailModalLabel');
+    modalTittle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML =`\
+    <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'Sorry Data Not Found'}</p>
+    <p>Chip Set: ${phone.mainFeatures.chipSet}`;
+}
+// loadPhone(apple);
